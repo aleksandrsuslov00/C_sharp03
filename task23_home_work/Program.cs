7ca94783320da1b5f5c4e9fbcbee295411313fa4
Aleksandr <a.suslov631@gmail.com>
2023-07-06 04:46:36 +0200

    task 23 homework - done

---
+++ task23_home_work/Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+Задача 23
+
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+
+*/
+
+Console.WriteLine("Input number ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int index = 0;
+int cube = 0;
+
+if (number > 0)
+{
+    while (index < number + 1)
+    {
+        cube = index * index * index;
+        index = index + 1;
+        Console.Write($"{cube}, ");
+    } 
+} 
+
+else 
+{
+    index = index * (-1); 
+    while (index > number - 1)
+    {
+        cube = index * index * index;
+        index = index - 1;
+        Console.Write($"{cube}, ");
+    }
+}
